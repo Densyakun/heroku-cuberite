3821c82b6c47e6b1193b8ac1d109a79b4b71f762
Densyakun <densyakun1128@gmail.com>
2022-06-02 04:59:13 +0000

    Add fastify middleware for webadmin

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const fastify = require('fastify')()
 fastify.register(require('@fastify/websocket'))
 const { toTCPOnConnection } = require('tcp-websocket-tunnel')
+await fastify.register(require('fastify-express'))
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 fastify.get('/ws', { websocket: true }, (connection, req) => toTCPOnConnection(connection.socket, 25565, 'localhost'))
+
+fastify.use(
+  '/webadmin',
+  createProxyMiddleware({
+    target: `http://localhost:${process.env.CUBERITE_WEBADMIN_PORT}`,
+    changeOrigin: true,
+  })
+)
 
 const start = async () => {
   try {
--- worker.sh
+++ worker.sh
@@ -1,4 +1,11 @@
 curl -LO https://download.cuberite.org/linux-x86_64/Cuberite.tar.gz
 tar xvf Cuberite.tar.gz
 
+echo "[WebAdmin]
+Ports=$CUBERITE_WEBADMIN_PORT
+Enabled=1
+
+[User:admin]
+Password=$CUBERITE_WEBADMIN_PASSWORD" >> webadmin.ini
+
 ./Cuberite

31e14e2c533dd0afda5f4763793b31fadc0c65ea
Densyakun <densyakun1128@gmail.com>
2022-06-02 07:17:07 +0000

    fix worker.sh

--- worker.sh
+++ worker.sh
@@ -3,11 +3,11 @@
 curl -LO https://download.cuberite.org/linux-x86_64/Cuberite.tar.gz
 tar xvf Cuberite.tar.gz
 
-echo "[WebAdmin]
-Ports=${$CUBERITE_WEBADMIN_PORT:-8080}
-Enabled=1
+echo "[User:admin]
+Password=$CUBERITE_WEBADMIN_PASSWORD
 
-[User:admin]
-Password=$CUBERITE_WEBADMIN_PASSWORD" >> webadmin.ini
+[WebAdmin]
+Ports=${CUBERITE_WEBADMIN_PORT:-8080}
+Enabled=1" > webadmin.ini
 
 ./Cuberite
